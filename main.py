7b15653abc2e563b94c31088fc9e8bf48a7ef55f
Agustin Montagni <agustin.montagni@gmail.com>
2022-03-21 22:31:35 -0300

    Calculo de latencia y área

--- main.py
+++ main.py
@@ -21,10 +21,10 @@ frecs = 20000       # Frecuencia de muestreo
 ##--- Files ----------------------------------------------------------------###
 # Remover comentario del archivo a analizar
 
-tp,yp = wp.getSignal("Datasets\\ncs_cmap_normal_a_elbow_2mvdiv_3msdiv.txt",f_sample=frecs)
+#tp,yp = wp.getSignal("Datasets\\ncs_cmap_normal_a_elbow_2mvdiv_3msdiv.txt",f_sample=frecs)
 #tp,yp = wp.getSignal("Datasets\\ncs_cmap_normal_b_elbow_2mvdiv_3msdiv.txt",f_sample=frecs)
 #tp,yp = wp.getSignal("Datasets\\ncs_cmap_normal_wrist_2mvdiv_3msdiv.txt",f_sample=frecs)
-#tp,yp = wp.getSignal("Datasets\\ncs_snap_normal_3rd_digit_10uvdiv_1msdiv.txt",f_sample=frecs)
+tp,yp = wp.getSignal("Datasets\\ncs_snap_normal_3rd_digit_10uvdiv_1msdiv.txt",f_sample=frecs)
 
 ##--- Calculos -------------------------------------------------------------###
 
@@ -33,7 +33,7 @@ ncs_sig1 = ncs.ncs_dataset()
 ncs_sig1.set_Dataset(dataset=yp, timespace = tp)
 
 # Seteo de threshold para la segmentación
-ncs_sig1.set_SegmentationThreshold(ncs_sig1.dataset.max() * 0.06,
+ncs_sig1.set_SegmentationThreshold(ncs_sig1.dataset.max() * 0.2,
                                     np.amax(np.diff(ncs_sig1.dataset)) * 0.11)
 
 # Computación de parámetros
@@ -47,8 +47,21 @@ axs[0].plot(tp,yp)
 
 axs[1].plot(tp,ncs_sig1.x_segm_mask * 0.005)
 
-for i in ncs_sig1.x_segments:
-    ti, yi = ncs_sig1.get_Time_Amp(i)
-    axs[2].plot(ti, yi)
+ta, ya = ncs_sig1.get_Time_Amp(ncs_sig1.stimulus(ncs_sig1.segmentos))
+axs[2].plot(ta, ya)
+
+print("Latencia Onset")
+print(ncs_sig1.t_onset_lat)
+#for i in ncs_sig1.segmentos:
+#    ti, yi = ncs_sig1.get_Time_Amp(i)
+#    axs[1].plot(ti, yi)
+
+
+
+#for i in ncs_sig1.x_segments:
+#    ti, yi = ncs_sig1.get_Time_Amp(i)
+#    axs[2].plot(ti, yi)
+
+#axs[2].axhline(ncs_sig1.y_baselines[1])
 
 plt.show()
--- ncs_study.py
+++ ncs_study.py
@@ -13,6 +13,7 @@
 import numpy as np
 import scipy.signal as sig
 import matplotlib.pyplot as plt
+import scipy.integrate as integ
 
 ##--- Classes --------------------------------------------------------------###
 
@@ -66,12 +67,21 @@ class ncs_dataset():
     correspondientes a los indices ingresados
     """
     def get_Time_Amp (self, xpoints):
-        t_val = []
+        try:
+            t_val = []
+            y_val = []
+            for point in xpoints:
+                y_val.append(self.dataset[point].copy())
+                t_val.append(self.timespace[point].copy())
+            return np.array(t_val), np.array(y_val)
+        except:
+            return np.nan, np.nan
+
+    def get_Amp (self, xpoints):
         y_val = []
         for point in xpoints:
             y_val.append(self.dataset[point].copy())
-            t_val.append(self.timespace[point].copy())
-        return np.array(t_val), np.array(y_val)
+        return np.array(y_val)
     
     """"
     Devuelve los valores del espacio lineal correspondientes a un valor 
@@ -85,6 +95,12 @@ class ncs_dataset():
             t_val.append(self.timespace[point].copy())
             y_val.append(ypoints)
         return t_val, y_val
+
+    """
+    Devuelve el tiempo entre dos muestras
+    """
+    def get_Duration(self,start, end):
+        return self.timespace[end] - self.timespace[start]
     
     ###################################################################
     #--- Functions
@@ -93,11 +109,32 @@ class ncs_dataset():
     Genera los cálculos necesarios para la extracción de caracteristicas
     """
     def compute (self):
-        self.x_max = self.max_values()
-        self.x_min = self.min_values()
-        self.x_slope = self.slopes()
-        self.x_segments, self.x_segm_mask = self.segmentation(self.segthld, self.dthld)
+
+        # Extracción de parámetros y segmentación
+        #self.x_max = self.max_values()
+        #self.x_min = self.min_values()
+        #self.x_slope = self.slopes()
+        self.x_segments, self.x_segm_mask, self.x_segments_div = self.segmentation(self.segthld, self.dthld)
+
+        # 
         self.y_baselines = self.baselines()
+        self.t_latencies = self.latencies()
+        self.x_mainsegment = self.mainsegment(self.segmentos)
+        self.x_stimulus = self.stimulus(self.segmentos)
+        self.t_onset_lat = self.onset_latency()
+        self.val_mainarea = self.mainarea()
+        self.v_area = self.mainarea()
+        pass
+        #
+
+    """
+    Devuelve la duración de cada segmento
+    """
+    def latencies(self):
+        durations = []
+        for segment in self.x_segments:
+            durations.append(self.get_Duration(segment[0],segment[-1]))
+        return durations
 
     """
     Devuelve segmentos de la señal basado en un threshold sobre la 
@@ -115,19 +152,81 @@ class ncs_dataset():
         
         lower_thld = np.argwhere(self.dataset < y_mean)
         lower_band = np.argwhere(self.dataset < y_mean_band[0])
-        lower_deriv = np.argwhere(np.absolute(np.diff(self.dataset)) > np.amax(np.diff(self.dataset)) * 0.11)
+        lower_deriv = np.argwhere(np.absolute(np.diff(self.dataset)) > d_thld)
 
         lower = np.union1d(np.intersect1d(lower_thld, lower_deriv), lower_band)
 
         segment_mask = np.zeros(len(self.dataset))
-        np.put(segment_mask, greater, 1)
         np.put(segment_mask, lower, -1)
-
+        np.put(segment_mask, greater, 1)
+        
+        segments_active = np.where(np.diff(segment_mask) > 0)[0]
         segments = np.where(np.diff(segment_mask))[0]
         
-        divided = np.split(range(len(self.dataset)),segments)
-                
-        return [divided, segment_mask]
+        segments_divided = np.split(range(len(self.dataset)),segments+1)
+        segments_active_divided = np.split(range(len(self.dataset)),segments_active+1)
+        
+        active = []
+        for seg in segments_active_divided:
+            if segment_mask[seg[0]:seg[-1]].all() != 0:
+                active.append(seg.copy().astype(int))
+        
+        self.segmentos = active
+
+        return [segments_divided, segment_mask, segments_active_divided]
+
+    """
+    Devuelve el segmento principal
+    """
+    def mainsegment (self, segments):
+        seg_len = [len(i) for i in segments]
+        ind = np.argmax(seg_len)
+        return segments[ind]
+
+    """
+    Devuelve el estímulo si existiera
+    """
+    def stimulus (self, segments):
+        if len(segments) == 1:
+            return np.nan
+        else:
+            seg_len = [len(i) for i in segments]
+            ind = np.argmin(seg_len)
+            return segments[ind]
+
+    """
+    Devuelve la latencia entre el estímulo o comienzo de la señal y el
+    segmento principal
+    """
+    def onset_latency (self):
+        if not np.isnan(self.x_stimulus):
+            t_lat = self.get_Duration(self.x_stimulus[-1], self.x_mainsegment[0])
+        else:
+            t_lat = self.get_Duration(0, self.x_mainsegment[0])
+        return t_lat
+
+    def mainarea (self):
+        if not np.isnan(self.x_stimulus):
+            amps = self.dataset[self.x_stimulus[-1] : self.x_mainsegment[0]]
+            v_med = np.mean(amps)
+            area = integ.simps(np.where(amps >= v_med))
+        else:
+            baseline = self.dataset[0 : self.x_mainsegment[0]]
+            v_med = np.mean(baseline)
+            amps = self.get_Amp(self.x_mainsegment)
+
+            segment = np.where(amps > v_med)
+            values = self.get_Amp(segment)
+
+            v_med_arr = np.full(len(segment), v_med)
+
+            area = integ.simps(values) - integ.simps(v_med_arr)
+            
+        print("Area positiva")
+        print(area)
+        return area
+
+        
 
     """
     Devuelve valores medios de los segmentos que no superan el threshold
@@ -141,9 +240,10 @@ class ncs_dataset():
                 means.append(np.mean(self.get_Time_Amp(base)[1]))
         return means        
 
+    ###########################################################################
     """
     Devuelve los puntos de la señal donde cambia el signo de
-    la pendiente
+    la pendiente (Sin utilizar)
     """
     def slopes (self):
         slope_section = []
@@ -155,7 +255,7 @@ class ncs_dataset():
 
     """
     Devuelve los puntos de la señal donde se cruza con el nivel
-    ingresado
+    ingresado. (Sin utilizar)
     """
     def crossing (self, level):
         x_val = np.where(
@@ -165,21 +265,21 @@ class ncs_dataset():
         return x_val
 
     """
-    Devuelve los puntos de la señal donde se detecta un máximo
+    Devuelve los puntos de la señal donde se detecta un máximo (Sin utilizar)
     """
     def max_values(self):
         x_val = sig.find_peaks(self.dataset)[0] 
         return x_val
         
     """
-    Devuelve los puntos de la señal donde se detecta un mínimo
+    Devuelve los puntos de la señal donde se detecta un mínimo (Sin utilizar)
     """
     def min_values(self):
         x_val = sig.find_peaks(self.inv_dataset)[0]
         return x_val
 
     """
-    Devuelve los anchos de los distintos máximos (TODO)
+    Devuelve los anchos de los distintos máximos (Sin utilizar)
     """
     def peak_widths(self):
         self.peak_width = sig.peak_widths(self.dataset, self.xmaxval, rel_height=1)
